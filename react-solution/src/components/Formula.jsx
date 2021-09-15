bc604be1eb7a68510da2187eff2f833e2c698075
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-15 19:56:21 +0200

    Create LOAD_PRODUCTS action type

--- react-solution/src/components/Formula.jsx
+++ react-solution/src/components/Formula.jsx
@@ -12,15 +12,25 @@ export default function Formula() {
     setCurrentFormulaState(formulaStates.EDIT);
   }
 
+  function handleSave() {
+
+  }
+  function handleCancel() {
+
+  }
+  function handleDelete() {
+
+  }
+
   return (
     <>
       <input type="text" />
       {currentFormulaState === formulaStates.EDIT
         ? (
           <>
-            <button type="button">Save</button>
-            <button type="button">Cancel</button>
-            <button type="button">Delete</button>
+            <button type="button" onClick={handleSave}>Save</button>
+            <button type="button" onClick={handleCancel}>Cancel</button>
+            <button type="button" onClick={handleDelete}>Delete</button>
           </>
         )
         : <button type="button" onClick={handleEdit}>Edit</button>}
