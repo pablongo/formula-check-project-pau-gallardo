5ad9aa9f9af40c6808b4317134e584a7c0aec307
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-19 11:15:12 +0200

    Add styles to navbar

--- react-solution/src/components/Navbar.jsx
+++ react-solution/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import './Navbar.css';
+
 export default function Navbar() {
   return (
-    <navbar>
+    <nav className="navbar">
       <a href="/">Logo</a>
       <a href="/">My Products</a>
       <a href="/">Prices formulas</a>
       <a href="/">Market position</a>
       <a href="/">Settings</a>
-    </navbar>
+    </nav>
   );
 }
