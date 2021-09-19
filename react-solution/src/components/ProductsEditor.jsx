b3be82df650d3ee5db20a150975b063ef2f76d25
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-19 10:55:39 +0200

    Add styles to ProductsEditor
    - Align content

--- react-solution/src/components/ProductsEditor.jsx
+++ react-solution/src/components/ProductsEditor.jsx
@@ -4,15 +4,17 @@ import React from 'react';
 import Formula from './Formula';
 import Product from './Product';
 
+import './ProductsEditor.css';
+
 export default function ProductsEditor({ products }) {
   return (
     <>
       {products?.map((product, index) => (
-        <>
+        <articles className="product-formula">
           <Product product={product} modified />
           <Formula product={product} index={index} />
           <Product product={product} modified={false} />
-        </>
+        </articles>
       ))}
     </>
   );
