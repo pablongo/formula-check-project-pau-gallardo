76ef0f0d3a639f78d9d23dca2c91193c4829406b
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-19 16:34:43 +0200

    Set up responsiveness

--- react-solution/src/components/Product.jsx
+++ react-solution/src/components/Product.jsx
@@ -5,19 +5,23 @@ import './Product.css';
 export default function Product({ product, modified }) {
   return (
     <div className="product-container">
-      <h2>{product?.name}</h2>
-      <img src="https://via.placeholder.com/150" alt="" />
+      <h2 className="product-container__title">{product?.name}</h2>
+      <img
+        className="product-container__image"
+        src="https://via.placeholder.com/150"
+        alt="product"
+      />
       {modified
         ? (
           <>
-            <h3>Original price</h3>
-            <span>{`${product?.price}€`}</span>
+            <h3 className="product-container__title">Original price</h3>
+            <span className="product-container__title">{`${product?.price}€`}</span>
           </>
         )
         : (
           <>
-            <h3>Modified price</h3>
-            <span>{`${product?.formulaPrice}€`}</span>
+            <h3 className="product-container__title">Modified price</h3>
+            <span className="product-container__title">{`${product?.formulaPrice}€`}</span>
           </>
         )}
     </div>
