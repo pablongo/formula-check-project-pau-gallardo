ed525b4c17409d9adea14ee5bde54143d134091c
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-20 11:04:16 +0200

    Set up formulaPage IFE

--- vanila-solution/pages/ApplyPriceFormulaPage/formulaPage.class.js
+++ vanila-solution/pages/ApplyPriceFormulaPage/formulaPage.class.js
@@ -2,4 +2,25 @@ class FormulaPage {
   constructor(products) {
     this.productList = products;
   }
+
+  setView() {
+    this.productList.forEach((product) => {
+      const element = `
+      <h1>${product.name}</h1>
+      <input
+      type="text"
+      className={validFormula ? 'formula-container__input' : 'formula-container__input--valid'}
+      name="formula-input"
+      placeholder={formulaPlaceholder}
+      value={inputFormula}
+      onChange={handleFormula}
+      disabled={currentFormulaState === formulaStates.DISPLAY}
+    />
+    `;
+
+      document.getElementById('product-list').innerHTML += element;
+    });
+  }
 }
+
+module.exports = FormulaPage;
