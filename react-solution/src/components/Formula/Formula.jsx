b5f034fe4204d1351b7014cb85168f378bd26591
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-21 15:30:35 +0200

    Refactor folder structure

--- react-solution/src/components/Formula/Formula.jsx
+++ react-solution/src/components/Formula/Formula.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { useDispatch } from 'react-redux';
 
-import { saveFormulaAndPrice } from '../redux/actions/actionCreators';
+import { saveFormulaAndPrice } from '../../redux/actions/actionCreators';
 
 import './Formula.css';
 
--- react-solution/src/components/ProductsEditor/ProductsEditor.jsx
+++ react-solution/src/components/ProductsEditor/ProductsEditor.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-import Formula from './Formula';
-import Product from './Product';
+import Formula from '../Formula/Formula';
+import Product from '../Product/Product';
 
 import './ProductsEditor.css';
 
